5bddc46483459750028657b1950416b89ab99141
Eric McCarthy <eric@limulus.net>
2013-09-04 12:43:10 -0700

    Basics.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 "use strict"
 
+var Compiler = module.exports.Compiler = function () {
+    this._closureCommandLineRunner = null
+}
+
+Compiler.prototype.setClosureCommandLineRunner = function (runner) {
+    this._closureCommandLineRunner = runner
+}
+
+Compiler.prototype.compile = function (job, cb) {
+    this._closureCommandLineRunner.call(global, [], cb)
+}
--- test/closured_test.js
+++ test/closured_test.js
@@ -1,2 +1,27 @@
 "use strict"
 
+var closured = require("../index.js")
+  , sinon = require("sinon")
+  , assert = require("assert")
+
+var compiler
+beforeEach(function () {
+    compiler = new closured.Compiler()
+})
+
+function runClosureCommandLineMockSuccess (args, cb) {
+    setImmediate(cb(null))
+}
+
+describe("#compile", function () {
+    var runner
+    beforeEach(function () {
+        runner = sinon.spy(runClosureCommandLineMockSuccess)
+        compiler.setClosureCommandLineRunner(runner)
+    })
+
+    it("should call the provided runClosureCommandLine function", function (done) {
+        compiler.compile(null, done)
+    })
+})
+
