af7abd994f1f7d36ad2970540d472e548ac1721c
Eric McCarthy <eric@limulus.net>
2013-10-07 16:15:50 -0700

    Copyright notice update.

--- test/CompilerProcessMock.js
+++ test/CompilerProcessMock.js
@@ -1,6 +1,5 @@
 /**
- * @license 
- * Copyright 2013 Eric McCarthy <eric@limulus.net>.
+ * @license Copyright 2013 DesertNet, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
